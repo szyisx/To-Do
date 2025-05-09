e6a7a854f3671b516d81130930ef8ba1aa015739
Szyisx <buldakoff.artiom2017@gmail.com>
2025-05-09 19:27:28 +0300

    Обновил проект: добавил Dockerfile и другие изменения

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 from routers import tasks
 from database import engine
 from models import Base
-
+import uvicorn
 
 Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 app.include_router(tasks.router, prefix="/tasks", tags=["Tasks"])
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0")
